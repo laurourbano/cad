7a617fe876b26fcc6ce1eb571cbc17faadbd5e3d
Lauro Otavio Urbano <lauro@outlook.com.br>
2023-12-22 22:10:32 -0300

    feat projeto com tailwind e tailblocks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { ContatoComponent } from './components/contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
